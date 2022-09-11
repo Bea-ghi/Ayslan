70d13bd37f751de6a745439b19601cfe73632926
Beatrici Schutz Ghidetti <88405377+Bea-ghi@users.noreply.github.com>
2022-09-11 15:38:24 -0300

    Add files via upload

---
+++ Ayslan/conexao.php
@@ -0,0 +1,8 @@
+<?php
+$hostname = "localhost";
+$user = "root";
+$password = "";
+$database = "trabalho";
+$connection = mysqli_connect ($hostname, $user, $password, $database);
+
+?>
---
+++ Ayslan/validar.php
@@ -0,0 +1,18 @@
+<?php 
+require_once("conexao.php");
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+
+$sql = "SELECT id FROM cliente WHERE (email= '$email') AND (senha = '$senha')";
+
+$query = mysqli_query ($connection, $sql);
+
+if (mysqli_num_rows($query) != 1){
+    header("Location: login.php");
+}
+else{
+header ("Location: index.php");
+}
+?>
